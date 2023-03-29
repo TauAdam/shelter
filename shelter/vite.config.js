1558532b28593185df491af2e8dd05d3a5e80986
Miras <miamib531@gmail.com>
2023-03-29 22:32:04 +0600

    feat: update pets page

--- shelter/vite.config.js
+++ shelter/vite.config.js
@@ -1,5 +1,14 @@
+import { resolve } from 'path'
 export default {
   server: {
     open: true,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'src/pets.html'),
+      },
+    },
   },
 }
