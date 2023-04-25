a96bf2561e91c14f4ad2d614aac401a63d968373
Miras <miamib531@gmail.com>
2023-04-25 20:40:36 +0600

    feat: add popup

--- shelter/main.js
+++ shelter/main.js
@@ -1,2 +1,21 @@
 import './src/index.scss'
-import './src/ui.js'
+
+const burgerButton = document.querySelector('.burger')
+const burgerMenu = document.querySelector('.burger__menu')
+const navBar = document.querySelector('.nav__menu').cloneNode(true)
+const overlay = document.querySelector('.overlay')
+
+burgerButton.addEventListener('click', () => {
+  burgerButton.classList.toggle('active')
+  burgerMenu.classList.toggle('active')
+  overlay.classList.toggle('active')
+  burgerMenu.append(navBar)
+})
+document.addEventListener('click', e => {
+  if (e.target.closest('.menu__link')) {
+    burgerButton.classList.remove('active')
+    burgerMenu.classList.remove('active')
+    overlay.classList.remove('active')
+  }
+})
+
